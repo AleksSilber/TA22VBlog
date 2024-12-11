007bedac1770f976afcbe431958afcc0937f4bd4
rab0ta <aleksei.silber@tptlive.ee>
2024-12-11 12:38:19 +0200

    Add tags & tags CRUD + add comments

--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,15 @@ use App\Http\Controllers\PostController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\PublicController;
 use App\Http\Controllers\UserController;
-
+use App\Http\Controllers\TagController;
+use App\Http\Controllers\CommentController;
 
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [PublicController::class, 'index'])->name('home');
 Route::get('/post/{post}', [PublicController::class, 'post'])->name('post');
 Route::get('/tag/{tag}', [PublicController::class, 'tag'])->name('tag');
+Route::get('/comment/{comment}', [PublicController::class, 'comment'])->name('comment');
 
 // Route::get('/admin/posts', [PostController::class, 'index'])->name('posts.index');
 // Route::get('/admin/posts/create', [PostController::class, 'create'])->name('posts.create');
@@ -29,6 +31,8 @@ Route::get('/dashboard', function () {
 Route::middleware('auth')->group(function () {
     Route::resource('/admin/posts', PostController::class);
     Route::resource('/admin/users', UserController::class);
+    Route::resource('/admin/comments', CommentController::class);
+    Route::resource('/admin/tags', TagController::class);
     Route::post('/post/{post}/like', [PublicController::class, 'like'])->name('like');
     
     Route::get('admin/user/{user}', [UserController::class, 'user'])->name('user');
@@ -40,6 +44,10 @@ Route::middleware('auth')->group(function () {
 
     
     Route::get('/admin/user/{user}', [UserController::class, 'user'])->name('user');
+
+    Route::get('/tags', [TagController::class, 'index'])->name('tags.index');
+    Route::get('/tags/create', [TagController::class, 'create'])->name('tags.create');
+    Route::post('/tags', [TagController::class, 'store'])->name('tags.store');
 
 
 });
